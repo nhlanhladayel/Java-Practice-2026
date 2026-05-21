a2708cf592336acdab525102d18498392cfa5148
Nhlanhla Mnume <rugbratsss@gmail.com>
2026-05-21 15:44:00 +0200

    Implement student grading input and processing

---
+++ grading-system/StudentGrading.java
@@ -0,0 +1,44 @@
+public static void main(String[] args){
+		Scanner kb = new Scanner(System.in);
+		
+		while(true){
+			System.out.print("Enter Student Number (-1 to Stop): ");
+			long studentNum = kb.nextLong();
+			kb.nextLine();
+			
+			if(studentNum == -1)
+			{
+				System.out.print("Exiting program");
+				break;
+			}
+			if(!validateStudentNumber(studentNum))
+			{
+				System.out.println("Invalid student number");
+				continue;
+			}
+			System.out.print("Enter course(Computer Science/Informations System/Software Engineering): ");
+			String courseName = kb.nextLine();
+			
+			if(!validCourse(courseName))
+			{
+				System.out.println("Invalid course! Try again");
+				continue;
+			}
+			System.out.print("Enter Final Mark: ");
+			int mark = kb.nextInt();
+			
+			if(!validateMark(mark))
+			{
+				System.out.print("Invalid mark");
+				continue;
+			}
+			String grade = determineGrade(mark);
+			recordMarks(studentNum,mark,courseName);
+			
+			System.out.println("Grade: " + grade);
+			
+		}
+		displayResults();
+		
+	}
+}
