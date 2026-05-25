bec581cc340b1a4fe07788168f628f329d786a64
Nhlanhla Mnume <rugbratsss@gmail.com>
2026-05-25 21:52:06 +0200

    Implement getInitials method in InitializedName class
    Add method to get initials from a full name.

--- character-and-string-manipulation/InitializedName.java
+++ character-and-string-manipulation/InitializedName.java
@@ -1,5 +1,17 @@
 import java.util.Scanner;
 public class InitializedName{
+	public static String getInitials(String names){
+		names = names.trim();
+		String[] parts = names.split(" ");
+		String initials = " ";
+		
+		String lastname = parts[parts.length -1];
+		for(int i = 0; i < parts.length - 1; i++){
+			initials += Character.toUpperCase(parts[i].charAt(0)) + ". ";
+		}
+		
+		return initials + lastname;
+	}
 	public static void main(String[] args){
 		Scanner kb = new Scanner(System.in);
 		
