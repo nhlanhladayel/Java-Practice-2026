ce10f0c3ce8413719adc509e5f41f67b5c259b0b
Nhlanhla Mnume <rugbratsss@gmail.com>
2026-05-25 23:15:13 +0200

    Update fileExtension.java

--- character-and-string-manipulation/fileExtension.java
+++ character-and-string-manipulation/fileExtension.java
@@ -1,4 +1,15 @@
-	public static void main(String[] args){
+	public static String getInitials(String fullnames){
+		fullnames = fullnames.trim();
+		String[] parts = fullnames.split(" ");
+		String initials = "";
+		String surname = parts[parts.length - 1];
+		
+		for(int i = 0; i < parts.length - 1; i++){
+			initials += Character.toUpperCase(parts[i].charAt(0));
+		}
+		return initials + surname;
+	}
+public static void main(String[] args){
 		Scanner kb = new Scanner(System.in);
 		String extension;
 		
