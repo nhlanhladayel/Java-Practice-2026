e0957361513e5eba8174e61424e80e66d442c769
Nhlanhla Mnume <rugbratsss@gmail.com>
2026-05-25 23:15:40 +0200

    Add student number validation method

--- character-and-string-manipulation/fileExtension.java
+++ character-and-string-manipulation/fileExtension.java
@@ -1,4 +1,14 @@
-	public static String getInitials(String fullnames){
+	public static boolean validateStudNum(long studNum){
+		String numStr = String.valueOf(studNum);
+		if(numStr.length() == 9 && numStr.substring(0,4).equals("2025") ){
+			System.out.println("Correct student number format!");
+			return true;
+		}else{
+			System.out.println("Invalid student number!");
+			return false;
+		}
+	}
+public static String getInitials(String fullnames){
 		fullnames = fullnames.trim();
 		String[] parts = fullnames.split(" ");
 		String initials = "";
