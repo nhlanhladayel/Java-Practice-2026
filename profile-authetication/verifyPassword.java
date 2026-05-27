65d3bae4ed66eead6b873868ca7d384cbd4f9dfd
Nhlanhla Mnume <rugbratsss@gmail.com>
2026-05-27 22:28:03 +0200

    Add password verification functionality
    Implements a password verification system that checks for length, upper case, lower case, digits, and special characters. Provides user feedback on password strength and allows for multiple checks.

---
+++ profile-authetication/verifyPassword.java
@@ -0,0 +1,54 @@
+	public static void main(String[] args){
+		Scanner kb = new Scanner(System.in);
+		
+		int lowerCount = 0;
+		int upperCount = 0;
+		int countDigit = 0; 
+		int countSpecial = 0;
+		
+		while(true){
+			System.out.print("Enter your password: ");
+			String password = kb.nextLine();
+		
+			if(password.length() < 8){
+				System.out.println("Password must be atleast 8 or more...");
+				continue;
+			}else{
+				boolean valid = checkUpper(password) && checkLower(password) && checkDigit(password) && checkSpecial(password);
+				if(valid){
+					System.out.println("Password id strong.");
+					int upperC = countUpper(password,upperCount);
+					int lowerC = countLower(password,lowerCount);
+					int speacialC = countSpecial(password,countSpecial);
+					int digitC = countDigit(password,countDigit);
+					System.out.println("Has " + upperC + " upper cases.");
+					System.out.println("Has " + lowerC + " lower cases.");
+					System.out.println("Has " + speacialC + " speacial cases.");
+					System.out.println("Has " + digitC + " digits.");
+					break;
+				}else{
+					if(!checkUpper(password)){
+						System.out.println("No upper case.");
+					}
+					if(!checkLower(password)){
+						System.out.println("No lower case.");
+					}
+					if(!checkDigit(password)){
+						System.out.println("No digits.");
+					}
+					if(!checkSpecial(password)){
+						System.out.println("No speacial case.");
+					}
+				}
+				System.out.print("Do you wish to continue checking? (y/n): ");
+				char check = kb.next().charAt(0);
+				kb.nextLine();
+				if(check == 'n' || check == 'N'){
+					System.out.println("Thank you.");
+					break;
+				}
+			}
+		}	
+	
+	}
+}
